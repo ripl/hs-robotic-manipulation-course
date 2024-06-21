2e95d345f6422d7d78b7f8cd3a97117bb6a5fa92
Geno Lewis <grlewis@uchicago.edu>
2024-06-21 12:38:13 -0500

    Structure

---
+++ python/foundations/expressions/examples/circle.py
@@ -0,0 +1,11 @@
+from math import pi
+
+def main():
+    """
+    Print some facts about a circle with radius 10.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ python/foundations/expressions/lab/question-4.py
@@ -0,0 +1,12 @@
+def plus_10(n):
+  """
+  Return n + 10
+  
+  Examples:
+  >>> plus_10(12)
+  22
+  >>> n = plus_10(3)
+  >>> n 
+  13
+  """
+  pass
