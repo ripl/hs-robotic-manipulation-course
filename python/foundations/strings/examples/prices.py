8490a824c67d49b479c91432f99984d33a952e92
luwop <146183277+luwop@users.noreply.github.com>
2024-06-23 21:23:00 -0500

    Update prices.py

--- python/foundations/strings/examples/prices.py
+++ python/foundations/strings/examples/prices.py
@@ -1,7 +1,15 @@
 def main():
     """
-    This program calculates the total price of items 
-    in a cart based on user input.
+    Prompt the user for the number of items in their cart then calculate the 
+    total price of items in a cart based on user input.
+
+    Example:
+    --------
+    How many items are in the cart? 3
+    Enter the price of item 1: 5.99
+    Enter the price of item 2: 3.50
+    Enter the price of item 3: 12.00
+    The total price of all items is: 21.49
     """
     pass
 
