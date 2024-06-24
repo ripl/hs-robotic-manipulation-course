f61cc90d96e146274bb0a6d05801b4ab0e028f4b
luwop <146183277+luwop@users.noreply.github.com>
2024-06-23 20:38:09 -0500

    Update func.py

---
+++ python/foundations/expressions/examples/func.py
@@ -0,0 +1,22 @@
+from operator import add, mul, neg
+
+def subtract(a, b):
+    """
+    Compute the difference between a and b.
+    """
+    pass
+
+def square(n):
+    """
+    Compute the square of n.
+    """
+    pass
+
+def main():
+  print()
+  print(12, 'squared is', square(12))
+  print('5 - 4 is', subtract(5, 4))
+  print()
+
+if __name__ == '__main__':
+  main()
