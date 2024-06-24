1a6f4bcb8901c9cf265c888c3fbc4fed11700a23
luwop <146183277+luwop@users.noreply.github.com>
2024-06-23 20:41:55 -0500

    Create question-6.py

---
+++ python/foundations/expressions/lab/question-6.py
@@ -0,0 +1,12 @@
+def repeat_3(s):
+    """
+    Using print, display 3 copies of s 
+    on a single line without spaces.
+    
+    >>> repeat_3("haha")
+    hahahahahaha
+
+    >>> repeat_3(10)
+    101010
+    """
+    pass
