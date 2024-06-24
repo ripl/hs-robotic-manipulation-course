469632794715bcf0944ccb3a8c3ea2b0a994913b
luwop <146183277+luwop@users.noreply.github.com>
2024-06-23 21:18:15 -0500

    Create square.py

---
+++ python/foundations/strings/examples/square.py
@@ -0,0 +1,14 @@
+def main():
+    """
+    This program prints a square made of stars.
+        
+    (i, j)  0 1 2 3    
+          0 * * * *
+          1 * * * *
+          2 * * * * 
+          3 * * * *
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
