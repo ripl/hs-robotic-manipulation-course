576e28eefe43205829e1cac9bb946e5e925259ab
luwop <146183277+luwop@users.noreply.github.com>
2024-06-23 20:40:23 -0500

    Create dice.py

---
+++ python/foundations/expressions/examples/dice.py
@@ -0,0 +1,15 @@
+import random
+
+NUM_SIDES = 6   # constant
+
+def roll_dice():
+    """
+    Simulate rolling two dice.
+    """
+    pass
+
+def main():
+    roll_dice()
+
+if __name__ == '__main__':
+    main()
