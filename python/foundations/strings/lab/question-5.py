37e87e3303a8c0366eec5d45431f710a83a4af6c
luwop <146183277+luwop@users.noreply.github.com>
2024-06-23 21:38:21 -0500

    Create question-5.py

---
+++ python/foundations/strings/lab/question-5.py
@@ -0,0 +1,14 @@
+def indoor(s):
+    """
+    Return the string s in lowercase.
+
+    >>> indoor('HELLO!')
+    'hello!'
+
+    >>> indoor('123')
+    '123'
+
+    >>> indoor('bEaCh DAY')
+    'beach day'
+    """
+    pass
