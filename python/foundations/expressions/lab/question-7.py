2d17d39c6069e320f4eb0e50fe9e066cedd563a6
luwop <146183277+luwop@users.noreply.github.com>
2024-06-24 06:26:28 -0500

    Update question-7.py

--- python/foundations/expressions/lab/question-7.py
+++ python/foundations/expressions/lab/question-7.py
@@ -1,16 +1,16 @@
 def sphere(radius):
 	"""
 	Compute and print the volume, surface area and circumfrence of a sphere 
-  given its radius, with 3 decimal precision.
+  	given its radius, with 3 decimal precision.
 
 	>>> sphere(3)
 	Volume: 113.097
-  Surface Area: 113.097
-  Circumfrence: 18.850
+  	Surface Area: 113.097
+  	Circumfrence: 18.850
 
 	>>> sphere(12)
 	Volume: 904.779
-  Surface Area = 452.389
-  Circumference = 37.6991
+  	Surface Area = 452.389
+  	Circumference = 37.6991
 	"""
 	pass
