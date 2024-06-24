7b998234760010d15e44c0d776238e9f7fe214c4
luwop <146183277+luwop@users.noreply.github.com>
2024-06-23 21:21:31 -0500

    Create distance.py

---
+++ python/foundations/strings/examples/distance.py
@@ -0,0 +1,18 @@
+import math
+
+def main():
+    """
+    Prompt the user to input the coordinates of two points (x1, y1) and (x2, y2).
+    Then, compute the distance using the formula:
+
+        √((x2 - x1)^2 + (y2 - y1)^2)
+
+    Example:
+    --------
+    Enter x_1: 1
+    Enter x_2: 4
+    Enter y_1: 2
+    Enter y_2: 6
+    The distance between the points (1, 2) and (4, 6) is 5.0
+    """
+    pass
