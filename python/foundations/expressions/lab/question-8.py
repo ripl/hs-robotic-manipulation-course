206fbe2fe77611718124131a446ecae033a87a37
luwop <146183277+luwop@users.noreply.github.com>
2024-06-23 20:44:57 -0500

    Create question-8.py

---
+++ python/foundations/expressions/lab/question-8.py
@@ -0,0 +1,19 @@
+from operator import mul, add
+
+def cube_plus_square(x):
+    """
+    Using ONLY the mul and add functions from the operator library, 
+    return the cube of x plus the square of x. 
+    
+    DO NOT use any arithmetic operators (+, *, **, etc...)
+
+    Note: mul and add are functions that multiply and sum two arguments. 
+    For example, mul(2, 4) is 8 and add(2, 2) is 4.
+
+    >>> cube_plus_square(10)
+    1100
+
+    >>> cube_plus_square(cube_plus_square(2))
+    1872
+    """
+    pass
