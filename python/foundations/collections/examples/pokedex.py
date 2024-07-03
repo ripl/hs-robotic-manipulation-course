4bf2a712ea15960daea97f35359d366efce7c348
Geno Lewis <grlewis@uchicago.edu>
2024-07-03 11:18:00 -0500

    Updated

--- python/foundations/collections/examples/pokedex.py
+++ python/foundations/collections/examples/pokedex.py
@@ -13,7 +13,18 @@ def count_rarity_levels(cards):
   >>> count_rarity_levels(cards)
   {'Common': 2, 'Rare': 3, 'Legendary': 1, 'Uncommon': 1}
   """
-  pass
+  result = {}
+
+  # Assumes I know all the rarities 
+  for pokemon in cards:
+    rarity = cards[pokemon] # Uncommon, Rare, etc...
+
+    if rarity in result:
+      result[rarity] += 1
+    else:
+      result[rarity] = 1
+
+  return result
 
 def filter_by_rarity(cards, rarity):
   """
@@ -35,7 +46,14 @@ def filter_by_rarity(cards, rarity):
   >>> filter_by_rarity(cards, "Mythical")  # Returns an empty dictionary for non-existent rarity
   {}
   """
-  pass
+  res = {}
+
+  # Option 1
+  for pokemon in cards:
+    if cards[pokemon] == rarity:
+      res[pokemon] = rarity
+
+  return res
 
 def sort_cards_by_rank(cards):
     """
@@ -55,6 +73,15 @@ def sort_cards_by_rank(cards):
         'Common': 1, 'Uncommon': 2, 
         'Rare': 3, 'Legendary': 4
     }
-    pass
+    # Step 1: Convert the dictionary into a lists of sublists, sublists = [key, value]
+    res = []
+    for key, value in cards.items():
 
+      pair = [key, value]
+
+      res.append(pair)
+
+    # Step 2: Sort that lists by "rank"
+
+    return sorted(res, key=lambda card: rank[card[1]], reverse=True)
 
--- python/foundations/collections/lab/practice.py
+++ python/foundations/collections/lab/practice.py
@@ -1,8 +1,14 @@
 def find_grades(grades, students):
     """
-    grades is a dictionary mapping student names (str) to grades (str).
-    students is a list of student names.
-    Returns a list containing the grades for students (in the same order).
+    Returns a list containing the grades for students (in the same order)
+
+    Inputs:
+        grades (dict): is a dictionary mapping student names (str) to grades (str).
+    
+        students (list): is a list of student names.
+
+    Returns:
+        list: a list of the grades in order by name pf students.
 
     Examples:
     >>> d = {'Ana': 'B', 'Matt': 'C', 'John': 'B', 'Katy': 'A'}
@@ -15,33 +21,45 @@ def find_grades(grades, students):
     >>> find_grades(d, ['John', 'Ana', 'Katy'])
     ['B', 'B', 'A']
     """
-    pass
+    res = []
+
+    for name in grades:
+        if name in students:
+            res.append(grades[name])
+
+    return res
 
 def new_dict(input_dict):
     """
     Takes a dictionary and returns a new dictionary where 5 is added to each value.
     
-    Args:
-    input_dict (dict): A dictionary with integer values.
+    Inputs:
+        input_dict (dict): A dictionary with integer values.
 
     Returns:
-    dict: A new dictionary with each value increased by 5.
+        dict: A new dictionary with each value increased by 5.
     
     Examples:
     >>> new_dict({"item1": 2, "item2": 7, "item3": 20})
     {'item1': 7, 'item2': 12, 'item3': 25}
     """
-    pass
+    res = {}
+
+    for key, value in input_dict.items():
+        res[key] = value + 5
+    
+    return res
+
 
 def check_same_values(input_dict):
     """
     Checks if all values in the dictionary are the same.
 
-    Args:
-    input_dict (dict): A dictionary with values.
+    Inputs:
+        input_dict (dict): A dictionary with values.
 
     Returns:
-    bool: True if all values are the same, False otherwise.
+        bool: True if all values are the same, False otherwise.
     
     Examples:
     >>> check_same_values({'item1': 'apple', 'item2': 'apple', 'item3': 'apple'})
@@ -55,15 +73,15 @@ def combine_dicts(input_dicts):
     """
     Combines a list of dictionaries into a single dictionary by summing the amounts of the same items.
     
-    Args:
-    input_dicts (list): A list of dictionaries with 'item' and 'amount' keys.
+    Inputs:
+        input_dicts (list): A list of dictionaries with 'item' and 'amount' keys.
 
     Returns:
-    dict: A combined dictionary with summed amounts for each item.
+        dict: A combined dictionary with summed amounts for each item.
     
     Examples:
-    >>> combine_dicts([{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300}, {'item': 'item1', 'amount': 750}])
-    {'item1': 1150, 'item2': 300}
+    >>> combine_dicts([{'item': 'servos', 'amount': 400}, {'item': 'breadboard', 'amount': 300}, {'item': 'servos', 'amount': 750}])
+    {'servos': 1150, 'breadboard': 300}
     """
     pass
 
